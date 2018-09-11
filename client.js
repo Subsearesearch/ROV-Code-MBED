0b9aa52e385bf7888abf5125d64063cb5a7aaba8
Noah Klayman <noahklayman@gmail.com>
2018-09-11 15:52:47 -0700

    fixed some stuff

--- client.js
+++ client.js
@@ -5,7 +5,14 @@ const socket = new net.Socket()
 socket.on('connect', () => {
   console.log('connected!')
   socket.write(
-    '{"ctrl":{"quat":{"w":0.3681245526846781,"x":0.9297764858882513,"y":0,"z":0},"lin":{"x":0,"y":0.45,"z":0}}}'
+    JSON.stringify({
+      ctrl: {
+        quat: { w: 0.3681245526846781, x: 0.9297764858882513, y: 0, z: 0 },
+        lin: { x: 0, y: 0.45, z: 0 },
+        pq: 1,
+        pw: 1
+      }
+    })
   )
 })
 socket.on('data', data => {
--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include "EthernetInterface.h"
 #include "TCPServer.h"
 #include "TCPSocket.h"
+#include "MbedJSONValue.h"
+#include <string>
+#include "Quaternion.h"
 
 #define HTTP_STATUS_LINE "HTTP/1.0 200 OK"
 #define HTTP_HEADER_FIELDS "Content-Type: text/html; charset=utf-8"
@@ -27,88 +30,95 @@
 
 DigitalOut led1(LED1);
 
-// Vector3 feedbackLoop(const Quaternion &setpoint, const Quaternion &orientation, const Vector3 &velocity, const float pq, const float pw)
-// {
-//     Quaternion error = setpoint.difference(orientation);
-//     return -pq * error.v - pw * velocity;
-// }
-
+Vector3 feedbackLoop(const Quaternion &setpoint, const Quaternion &orientation, const Vector3 &velocity, const float pq, const float pw)
+{
+  Quaternion error = setpoint.difference(orientation);
+  return -pq * error.v - pw * velocity;
+}
 void cb()
 {
-    led1 = !led1;
+  led1 = !led1;
 }
 
 int main()
 {
-    printf("Basic HTTP server example\n");
+  printf("Basic HTTP server example\n");
 
-    EthernetInterface eth;
+  EthernetInterface eth;
 
-    eth.disconnect();
+  eth.disconnect();
 
-    eth.set_network("192.168.1.191", "255.255.255.0", "192.168.1.1");
+  eth.set_network("192.168.1.191", "255.255.255.0", "192.168.1.1");
 
-    // eth.attach(&cb);
-    // eth.set_blocking(false);
+  // eth.attach(&cb);
+  // eth.set_blocking(false);
 
-    eth.connect();
+  eth.connect();
 
-    printf("The Server IP address is '%s'\n", eth.get_ip_address());
-    TCPServer srv;
-    TCPSocket client_sock;
-    SocketAddress client_addr;
-    char buffer[256];
+  printf("The Server IP address is '%s'\n", eth.get_ip_address());
+  TCPServer srv;
+  TCPSocket client_sock;
+  SocketAddress client_addr;
+  char buffer[256];
 
-    /* Open the server on ethernet stack */
-    srv.open(&eth);
+  /* Open the server on ethernet stack */
+  srv.open(&eth);
 
-    /* Bind the HTTP port (TCP 80) to the server */
-    srv.bind(eth.get_ip_address(), 23);
+  /* Bind the HTTP port (TCP 80) to the server */
+  srv.bind(eth.get_ip_address(), 23);
 
-    /* Can handle x simultaneous connections */
-    srv.listen(1);
+  /* Can handle x simultaneous connections */
+  srv.listen(1);
 
-    srv.accept(&client_sock, &client_addr);
-    printf("Accepted %s:%d\n", client_addr.get_ip_address(),
-           client_addr.get_port());
-    strcpy(buffer, "Hello \n\r");
-    client_sock.send(buffer, strlen(buffer));
-    int ret;
-    while (true)
-    {
-        ret = client_sock.recv(buffer, sizeof(buffer) - 1);
-        if (ret <= 0)
-            break;
-        buffer[ret] = '\0';
-        json data = buffer;
-        printf("%s\n", data["ctrl"]);
-        // printf("Received %d chars from server:\n%s\n", ret, buffer); //["ctrl"]);
-    }
-    // client_sock.close();
-    // delete[] buffer;
-    while (1)
-    {
-    }
+  srv.accept(&client_sock, &client_addr);
+  printf("Accepted %s:%d\n", client_addr.get_ip_address(),
+         client_addr.get_port());
+  strcpy(buffer, "Hello \n\r");
+  client_sock.send(buffer, strlen(buffer));
+  int ret;
+  while (true)
+  {
+    ret = client_sock.recv(buffer, sizeof(buffer) - 1);
+    if (ret <= 0)
+      break;
+    buffer[ret] = '\0';
+    // printf("%s\n", buffer);
+    MbedJSONValue data;
+    const char *json = &buffer[ret];
+    bool my_bool;
+    //parse the previous string and fill the object data
+    parse(data, json);
+    Quaternion quat = data["ctrl"]["quat"];
+    my_bool = data["ctrl"]["quat"]["y"].get<bool>();
+    feedbackLoop(quat, quat, data["ctrl"]["pw"], data["ctrl"]["pq"]);
+    printf("my_bool: %s\r\n", my_bool ? "true" : "false");
+    // printf("Received %d chars from server:\n%s\n", ret, buffer); //["ctrl"]);
+  }
+  // client_sock.close();
+  // delete[] buffer;
+  while (1)
+  {
+  }
 
-    // printf("The target IP address is '%s'\n", eth.get_ip_address());
+  // printf("The target IP address is '%s'\n", eth.get_ip_address());
 
-    // TCPServer srv;
-    // TCPSocket clt_sock;
-    // SocketAddress clt_addr;
+  // TCPServer srv;
+  // TCPSocket clt_sock;
+  // SocketAddress clt_addr;
 
-    // /* Open the server on ethernet stack */
-    // srv.open(&eth);
+  // /* Open the server on ethernet stack */
+  // srv.open(&eth);
 
-    // /* Bind the HTTP port (TCP 80) to the server */
-    // srv.bind(eth.get_ip_address(), 80);
+  // /* Bind the HTTP port (TCP 80) to the server */
+  // srv.bind(eth.get_ip_address(), 80);
 
-    // /* Can handle 5 simultaneous connections */
-    // srv.listen(5);
+  // /* Can handle 5 simultaneous connections */
+  // srv.listen(5);
 
-    // while (true)
-    // {
-    //     srv.accept(&clt_sock, &clt_addr);
-    //     printf("accept %s:%d\n", clt_addr.get_ip_address(), clt_addr.get_port());
-    //     clt_sock.send(HTTP_RESPONSE, strlen(HTTP_RESPONSE));
-    // }
+  // while (true)
+  // {
+  //     srv.accept(&clt_sock, &clt_addr);
+  //     printf("accept %s:%d\n", clt_addr.get_ip_address(), clt_addr.get_port());
+  //     clt_sock.send(HTTP_RESPONSE, strlen(HTTP_RESPONSE));
+  // }
 }
