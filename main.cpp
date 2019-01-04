3ea7ad5da234aa2ca1777eefb61abfdaf69e3429
Noah Klayman <noahklayman@gmail.com>
2019-01-04 11:58:05 -0800

    feat(control): support controlling individual thrusters

--- main.cpp
+++ main.cpp
@@ -90,119 +90,144 @@ int main()
     // printf("%s\n", buffer);
     const char *json = &buffer[ret];
     parse(data, buffer);
-    // Values are between -100 and 100
-    int x_lin = data["ctrl"]["xLin"].get<int>();
-    int y_lin = data["ctrl"]["yLin"].get<int>();
-    int z_lin = data["ctrl"]["zLin"].get<int>();
-    int x_rot = data["ctrl"]["xRot"].get<int>();
-    int y_rot = data["ctrl"]["yRot"].get<int>();
-    int z_rot = data["ctrl"]["zRot"].get<int>();
-    // printf("%d\n", z_rot);
-    int bl = 0;
-    int br = 0;
-    int fl = 0;
-    int fr = 0;
-    // z_lin (up and down)
-    bl += z_lin;
-    br += z_lin;
-    fl += z_lin;
-    fr += z_lin;
-    // y_rot (tilt left (-) and right (+))
-    fr -= y_rot;
-    br -= y_rot;
-    bl += y_rot;
-    fl += y_rot;
-    // x_rot (tilt up (-) and down (+))
-    fr -= x_rot;
-    fl -= x_rot;
-    br += x_rot;
-    bl += x_rot;
-    // make sure highest is 100
-    int max;
-    max = fmax(fmax(abs(fr), abs(fl)), fmax(abs(br), abs(bl)));
-    if (max > 100)
-    {
-      fr /= max;
-      fl /= max;
-      br /= max;
-      bl /= max;
-    }
-    int f = 0;
-    int b = 0;
-    int l = 0;
-    int r = 0;
+    // Individual or group control
+    bool is_individual = !(data["isGroup"].get<bool>());
 
-    // z_rot (+ is clockwise)
-    f += z_rot;
-    b -= z_rot;
-    l += z_rot;
-    r -= z_rot;
-    // x_lin (side to side, + is right)
-    f += x_lin;
-    b += x_lin;
-    // y_lin (front and back, + is forward)
-    l += y_lin;
-    r += y_lin;
-    // make sure highest is 100
-    max = fmax(fmax(abs(f), abs(b)), fmax(abs(r), abs(l)));
+    if (is_individual)
+    {
+      int f_val = data["individualCtrl"]["f"].get<int>();
+      int b_val = data["individualCtrl"]["b"].get<int>();
+      int l_val = data["individualCtrl"]["l"].get<int>();
+      int r_val = data["individualCtrl"]["r"].get<int>();
+      int fl_val = data["individualCtrl"]["fl"].get<int>();
+      int fr_val = data["individualCtrl"]["fr"].get<int>();
+      int bl_val = data["individualCtrl"]["bl"].get<int>();
+      int br_val = data["individualCtrl"]["br"].get<int>();
+      f_pwm.pulsewidth_us(4 * f_val + 1492);
+      b_pwm.pulsewidth_us(4 * b_val + 1494);
+      l_pwm.pulsewidth_us(4 * l_val + 1491);
+      r_pwm.pulsewidth_us(4 * r_val + 1494);
+      fl_pwm.pulsewidth_us(4 * fl_val + 1492);
+      fr_pwm.pulsewidth_us(4 * fr_val + 1492);
+      bl_pwm.pulsewidth_us(4 * bl_val + 1491);
+      br_pwm.pulsewidth_us(4 * br_val + 1492);
+    }
+    else
+    {
+      // Values are between -100 and 100
+      int x_lin = data["ctrl"]["xLin"].get<int>();
+      int y_lin = data["ctrl"]["yLin"].get<int>();
+      int z_lin = data["ctrl"]["zLin"].get<int>();
+      int x_rot = data["ctrl"]["xRot"].get<int>();
+      int y_rot = data["ctrl"]["yRot"].get<int>();
+      int z_rot = data["ctrl"]["zRot"].get<int>();
+      // printf("%d\n", z_rot);
+      int bl = 0;
+      int br = 0;
+      int fl = 0;
+      int fr = 0;
+      // z_lin (up and down)
+      bl += z_lin;
+      br += z_lin;
+      fl += z_lin;
+      fr += z_lin;
+      // y_rot (tilt left (-) and right (+))
+      fr -= y_rot;
+      br -= y_rot;
+      bl += y_rot;
+      fl += y_rot;
+      // x_rot (tilt up (-) and down (+))
+      fr -= x_rot;
+      fl -= x_rot;
+      br += x_rot;
+      bl += x_rot;
+      // make sure highest is 100
+      int max;
+      max = fmax(fmax(abs(fr), abs(fl)), fmax(abs(br), abs(bl)));
+      if (max > 100)
+      {
+        fr /= max;
+        fl /= max;
+        br /= max;
+        bl /= max;
+      }
+      int f = 0;
+      int b = 0;
+      int l = 0;
+      int r = 0;
 
-    if (max > 100)
-    {
-      f /= max;
-      b /= max;
-      r /= max;
-      l /= max;
-    }
+      // z_rot (+ is clockwise)
+      f += z_rot;
+      b -= z_rot;
+      l += z_rot;
+      r -= z_rot;
+      // x_lin (side to side, + is right)
+      f += x_lin;
+      b += x_lin;
+      // y_lin (front and back, + is forward)
+      l += y_lin;
+      r += y_lin;
+      // make sure highest is 100
+      max = fmax(fmax(abs(f), abs(b)), fmax(abs(r), abs(l)));
 
-    // Convert to micro seconds (for pwm)
-    f = (4 * f + 1492);
-    b = (4 * b + 1494);
-    l = (4 * l + 1491);
-    r = (4 * r + 1494);
-    fr = (4 * fr + 1492);
-    fl = (4 * fl + 1492);
-    br = (4 * br + 1491);
-    bl = (4 * bl + 1492);
-    // printf("%d\n", fr);
-    if (last_f != f)
-    {
-      f_pwm.pulsewidth_us(f);
-      last_f = f;
-    }
-    if (last_b != b)
-    {
-      b_pwm.pulsewidth_us(b);
-      last_b = b;
-    }
-    if (last_l != l)
-    {
-      l_pwm.pulsewidth_us(l);
-      last_l = l;
-    }
-    if (last_r != r)
-    {
-      r_pwm.pulsewidth_us(r);
-      last_r = r;
-    }
-    if (last_fl != fl)
-    {
-      fl_pwm.pulsewidth_us(fl);
-      last_fl = fl;
-    }
-    if (last_fr != fr)
-    {
-      fr_pwm.pulsewidth_us(fr);
-      last_fr = fr;
-    }
-    if (last_bl != bl)
-    {
-      bl_pwm.pulsewidth_us(bl);
-      last_bl = bl;
-    }
-    if (last_br != br)
-    {
-      br_pwm.pulsewidth_us(br);
-      last_br = br;
+      if (max > 100)
+      {
+        f /= max;
+        b /= max;
+        r /= max;
+        l /= max;
+      }
+
+      // Convert to micro seconds (for pwm)
+      f = (4 * f + 1492);
+      b = (4 * b + 1494);
+      l = (4 * l + 1491);
+      r = (4 * r + 1494);
+      fr = (4 * fr + 1492);
+      fl = (4 * fl + 1492);
+      br = (4 * br + 1491);
+      bl = (4 * bl + 1492);
+      // printf("%d\n", fr);
+      if (last_f != f)
+      {
+        f_pwm.pulsewidth_us(f);
+        last_f = f;
+      }
+      if (last_b != b)
+      {
+        b_pwm.pulsewidth_us(b);
+        last_b = b;
+      }
+      if (last_l != l)
+      {
+        l_pwm.pulsewidth_us(l);
+        last_l = l;
+      }
+      if (last_r != r)
+      {
+        r_pwm.pulsewidth_us(r);
+        last_r = r;
+      }
+      if (last_fl != fl)
+      {
+        fl_pwm.pulsewidth_us(fl);
+        last_fl = fl;
+      }
+      if (last_fr != fr)
+      {
+        fr_pwm.pulsewidth_us(fr);
+        last_fr = fr;
+      }
+      if (last_bl != bl)
+      {
+        bl_pwm.pulsewidth_us(bl);
+        last_bl = bl;
+      }
+      if (last_br != br)
+      {
+        br_pwm.pulsewidth_us(br);
+        last_br = br;
+      }
     }
   }
   // client_sock.close();
